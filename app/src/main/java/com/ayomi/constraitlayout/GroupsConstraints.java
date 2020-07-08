cd0f613ecc92773c6b751a232ac5d6d8ad990c5b
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-07-08 01:50:43 +0100

    progress

---
+++ app/src/main/java/com/ayomi/constraitlayout/GroupsConstraints.java
@@ -0,0 +1,14 @@
+package com.ayomi.constraitlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupsConstraints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups_constraints);
+    }
+}
